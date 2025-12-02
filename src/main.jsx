0053f1ccabacdda748a910466a3095b4393e736b
MarcusEcko <marco237.mnm@gmail.com>
2025-12-02 20:09:58 -0300

    fix router I Hope

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { CartProvider } from './Context/CartProvider.jsx';
 import { ProductsProvider } from './Context/MockApiContext.jsx';
 import { AuthProvider } from './Context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ProductsProvider>
         <AuthProvider>
           <CartProvider>
@@ -18,6 +18,6 @@ createRoot(document.getElementById('root')).render(
           </CartProvider>
         </AuthProvider>
       </ProductsProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
